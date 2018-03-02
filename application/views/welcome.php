d00e1631cf3e1f253c109273a960953e311a6ff3
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-03-02 15:50:50 +0800

    fixed hamburger icon

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -16,7 +16,7 @@
       <!-- navigation.nunjucks -->
 <link rel="stylesheet" type="text/css" href="assets/css/welcome.css">
 
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="#"><img src="assets/images/payak-logo-blue-40.png" alt="Payak logo"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
