ec5fc6ac83af194b89d85edff4bbdf4b7f556900
martslizardo <antoniomartin.lizardo@yahoo.com>
2018-03-04 20:59:28 +0800

    fixed base url

--- application/config/config.php
+++ application/config/config.php
@@ -29,12 +29,12 @@ switch (ENVIRONMENT) {
 	case 'development':
 		$config['base_url'] = 'http://localhost/resto/';
 		break;
-	// case 'testing':
-	// 	$config['base_url'] = 'http://stage.payakkaps.com/';
-	// 	break;
-	// case 'production':
-	// 	$config['base_url'] = 'http://payakapps.com/';
-	// 	break;
+	case 'testing':
+		$config['base_url'] = 'http://stage.payakkaps.com/';
+		break;
+	case 'production':
+		$config['base_url'] = 'http://restoshift.com/';
+		break;
 }
 
 /*
